7f9a02fd0d53537765a8a8e0f001c54fd633168d
Gwendy <90543386+gwendylune@users.noreply.github.com>
2021-09-12 13:30:09 +0800

    Add files via upload

---
+++ ScriptableObj/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerStatus", menuName = "Data_PlayerStorage/PlayerStatus")]
+
+public class PlayerData : ScriptableObject
+{
+    public string PlayerName;
+    public int Player_maxHp;
+    public int Player_currentHp;
+    //public bool isStart=true;
+    //[TextArea]
+    //public string itemInfo;
+
+}
